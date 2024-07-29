3c87ca81571c75400bcf02f4a6086e2142a8d4e9
Dionne Memie <d.memie@gmail.com>
2024-07-29 13:48:22 +0100

    requests completed

--- app.js
+++ app.js
@@ -35,14 +35,31 @@ app.get("/api/recipes/:id", (req, res) => {
 
 //POST	/api/recipes	{ body }	create a new recipe	{ success: Boolean, payload: recipe }
 
-app.post("api/recipes", (req, res) => {
+app.post("/api/recipes", (req, res) => {
   const newRecipe = req.body.newRecipe
-  const recipe = {
-    ...newRecipe, 
-    date: Date.now()
-  }
 
-  recipes.push(recipe);
+  recipes.push(newRecipe);
   res.status(200).json({ "success": true, "payload": "recipe array", recipes})
   console.log(recipes);
 })
+
+// PATCH	/api/recipes/:id	{ body }	updated recipe	{ success: Boolean, payload: recipe }
+
+app.patch("/api/recipes/:id", (req, res) => {
+  const updatedRecipe = updateRecipeByID(
+    req.params.id,
+    req.body.recipes
+  );
+  res.status(200).json({ "success": true, "payload": "recipe array", recipes})
+}
+)
+// DELETE	/api/recipes/:id		recipe deleted	{ success: Boolean, payload: recipe }
+
+app.delete("/api/recipes/:id", (req, res) => {
+  const deleteRecipe = deleteRecipeByID(
+    req.params.id, 
+    req.body.recipes
+  );
+  res.status(200).json({ "success": true, "payload": "recipe array", recipes})
+}
+)
